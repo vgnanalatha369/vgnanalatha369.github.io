7652cca8c624532b2af113df8d7f8120b50277e9
vgnanalatha369 <vgnanalatha30@gmail.com>
2026-06-11 16:01:27 +0530

    update

---
+++ script.js
@@ -0,0 +1,35 @@
+let name="Ammu";
+var age=20;
+const height="5.5ft";
+console.log(name);
+console.log(age);
+console.log(height);
+console.log(age+5);
+console.log("My name is " +name+ " , my age is " +age+ " and my height is " +height);
+console.log(age*2);
+age+=3;
+console.log(age);
+age-=2;
+console.log(age);
+let age1=25;
+console.log(age1>age);
+console.log(age1<age);
+console.log(age1>=age);
+console.log(age1<=age);
+let food="Biryani";
+let drink="Coke";
+if(food==drink){
+    console.log("Food and drink are the same");
+}
+else{
+    console.log(+food+ " was my food today and " +drink+ " was my drink today");
+}
+let amount=3500;
+let five_hundred_notes=amount/500;
+let two_hundred_notes=amount/200;
+let hundred_notes=amount/100;
+console.log("Number of 500 notes: " +five_hundred_notes);
+console.log("Number of 200 notes: " +two_hundred_notes);
+console.log("Number of 100 notes: " +hundred_notes);*/
+
+
